2b2c9f6affd4274973f4445f7fbda8f379140053
Sqwezzy33333 <sergey.kornev3@yandex.by>
2023-02-01 12:46:56 +0300

    feat: add templates

---
+++ src/templates/pages.ts
@@ -0,0 +1,23 @@
+export const enum PageIds{
+  MainPage = 'main-page',
+  SettingsPage = 'settings-page'
+}
+
+export abstract class Page{
+  protected container: HTMLElement;
+  static TextObject = {
+  };
+  constructor(id: string) {
+    this.container = document.createElement("div");
+    this.container.id = id;
+  }
+  protected createHeaderTitle(text: string): HTMLElement {
+    const headerTitle = document.createElement("h1");
+    headerTitle.innerText = text;
+    return headerTitle;
+  }
+  render(): HTMLElement {
+    
+    return this.container;
+  }
+}
